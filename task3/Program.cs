8fe8f9640097bad1157dad539973d701b9ad7975
Anastasia <nastya-shishkina-1999@mail.ru>
2022-10-19 00:24:52 +0300

    task 3

---
+++ task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// 	3. Задать номер четверти, показать диапазоны для возможных координат
+
+Console.Clear();
+Console.Write("Введите номер четверти: ");
+int number = int.Parse(Console.ReadLine()!);
+if( number == 1)
+{
+    Console.WriteLine("Диапозоны возможных координат: x > 0 и y > 0");
+}
+if(number == 2)
+{
+    Console.WriteLine("Диапозоны возможных координат: x < 0, а y > 0");
+}
+if(number == 3)
+{
+    Console.WriteLine("Диапозоны возможных координат: x < 0 и y < 0");
+}
+if(number == 4)
+{
+    Console.WriteLine("Диапозоны возможных координат: x > 0, а y < 0");
+}
