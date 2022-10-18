7fbf6c439ff15c66c48c61af306d8d7a7919530d
Anastasia <nastya-shishkina-1999@mail.ru>
2022-10-19 00:24:39 +0300

    task 2

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+Console.Clear();
+Console.WriteLine("Программа, которая проверяет, является ли одно заданное Вами число квадратом другого, заданного Вами часла");
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+int sqr = number2 * number2;
+if (sqr == number1)
+{
+Console.WriteLine("Да");
+}
+else
+{
+Console.WriteLine("Нет");
+}
